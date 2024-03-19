53d723cfdcb47df47e1a072dfe03c5431313a755
Mayki Neri <maykineri@gmail.com>
2024-03-19 12:06:46 +0800

    Delete Modal

--- src/DeleteConfirmation.jsx
+++ src/DeleteConfirmation.jsx
@@ -5,10 +5,14 @@ const DeleteConfirmation = ({ isOpen, onClose, onDelete }) => {
         <div className={`modal ${isOpen ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: isOpen ? 'block' : 'none' }}>
             <div className="modal-dialog modal-dialog-centered" role="document" style={{ maxWidth: "350px" }}>
                 <div className="modal-content">
+                <div className="modal-header">
+                        <h5 className="modal-title">Confirm Delete Task</h5>
+                        <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
+                    </div>
                     <div className="modal-body" style={{ padding: "35px" }}>
                         Are you sure you want to delete this task?
                     </div>
-                    <div className="modal-footer" style={{ borderTop: "none", paddingBottom: "0" }}>
+                    <div className="modal-footer">
                         <button type="button" className="btn btn-primary DeleteYes" onClick={onDelete}>Yes</button>
                         <button type="button" className="btn btn-secondary DeleteNo" onClick={onClose}>No</button>
                     </div>
